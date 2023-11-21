c731e3b7a032014fbd21052ebc5774cdfe98a86c
Lívia Oliveira <148345773+livvrrs@users.noreply.github.com>
2023-11-21 18:18:59 -0300

    Update faleconoscobd.php

--- banco de dados/faleconoscobd.php
+++ banco de dados/faleconoscobd.php
@@ -1,5 +1,26 @@
 <?php
 
+// conexão com o localhost
+
+$localhost = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "projetofinal";
+
+// checa a conexão
+
+$conexao = new mysqli($localhost, $usuario, $senha, $banco);
+
+    if ($conexao->connect_errno) {
+
+        echo "Erro de conexão";
+    }
+    
+    else {
+
+        echo "Conexão efetuada com sucesso! \n"; 
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $nome = $_POST["nome"];
@@ -7,13 +28,13 @@
         $assunto = $_POST["assunto"];
         $mensagem = $_POST["mensagem"];
 
-// coloca os dados na tabela "adocao"
+// coloca os dados na tabela "faleconosco"
 
     $sql = "INSERT INTO faleconosco (nome, email, assunto, mensagem) VALUES ('$nome', '$email', '$assunto', '$mensagem')";
 
         if ($conexao->query($sql) === TRUE) {
 
-            echo "Envio concluído com sucesso! Aguarde para mais informações.";
+            echo "Envio efetuado com sucesso! Aguarde para  mais informações.";
 
         } else {
 
