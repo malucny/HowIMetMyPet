093eebac7302b70a6dd7414c266ac3117c6101fc
Maria Luísa Costa Nery <106162492+malucny@users.noreply.github.com>
2023-11-21 20:47:56 -0300

    Update recebido.php

--- recebido.php
+++ recebido.php
@@ -4,6 +4,7 @@ $email = $_REQUEST ['email'];
 $assunto = $_REQUEST ['assunto'];
 $mensagem = $_REQUEST ['mensagem'];
 
+//mensagem de sucesso após envio bem sucedido!
 echo "<h2> Olá $nome, nós da equipe How I Met My Pet agradecemos sua mensagem! </h2>";
 ?>
 <div style="font-family: Arial, Helvetica, sans-serif"></div>
