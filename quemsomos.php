710301dde85fdf3698f247ac462a2a42706e1e4c
Maria Luísa Costa Nery <106162492+malucny@users.noreply.github.com>
2023-11-21 20:47:00 -0300

    Update final quemsomos.php

--- quemsomos.php
+++ quemsomos.php
@@ -1,36 +1,35 @@
-
 <div style="border-top-style:solid; border-color:#f7ee65; text-align:center;">
-  <h2 style= "font-size: 25px; text-align:center; font-weight:bolder; color:#4272bc">QUEM SOMOS?</h2>
+  <h2 style= "text-align:center; font-weight:bolder; color:#4272bc">QUEM SOMOS?</h2>
   
-  <p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray">Seja bem-vindo ao nosso serviço de adoção de animais, um espaço dedicado a unir corações humanos e patinhas carentes. 
-  <br>Aqui, a busca por um amigo de quatro patas se transforma em uma jornada repleta de amor, compaixão e alegria!</br></p>
+  <h4 style= "text-align:center; font-weight:bold; color:darkslategray">Seja bem-vindo ao nosso serviço de adoção de animais, um espaço dedicado a unir corações humanos e patinhas carentes. 
+  <br>Aqui, a busca por um amigo de quatro patas se transforma em uma jornada repleta de amor, compaixão e alegria!</br></h4>
 
 <br></br>
 
-<p style= "font-size: 25px; text-align:center; font-weight:bolder; color:#4272bc">A EQUIPE POR TRÁS DO HOW I MET MY PET:</p>
+<h2 style= "text-align:center; font-weight:bolder; color:#4272bc">A EQUIPE POR TRÁS DO HOW I MET MY PET:</h2>
 
 
 <img src= "liviaa.png" width="350">
-<p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray"> Lívia Maria </p>
+<h4 style= "text-align:center; font-weight:bold; color:darkslategray"> Lívia Maria </h4>
 
 <br></br>
 
 <img src= "leticia.png" width="350">
-<p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray"> Letícia Almeida</p>
+<h4 style= "text-align:center; font-weight:bold; color:darkslategray"> Letícia Almeida</h4>
 
 <br></br>
 
 <img src= "luisaa.png" width="350">
-<p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray"> Maria Luísa</p>
+<h4 style= "text-align:center; font-weight:bold; color:darkslategray"> Maria Luísa</h4>
 
 <br></br>
 
 <img src= "joyce.png" width="350">
-<p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray"> Joyce Maria</p>
+<h4 style= "text-align:center; font-weight:bold; color:darkslategray"> Joyce Maria</h4>
 
 <br></br>
 
 <img src= "alanna.png" width="350">
-<p style= "font-size: 18px; text-align:center; font-weight:bold; color:darkslategray"> Alanna Maria </p>
+<h4 style= "text-align:center; font-weight:bold; color:darkslategray"> Alanna Maria </h4>
 
 </div>
